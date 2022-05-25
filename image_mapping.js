e462d3babe2d30c52c6dcb43d8dfff8594b0815c
tachysheurisko <crypto@j-loewe.de>
2022-05-25 19:12:08 +0200

    commited server changes to ipfs

--- image_mapping.js
+++ image_mapping.js
@@ -1,43 +1,41 @@
-const coatingList = ['None', 'Brown', 'White']
+const coatingList = ["None", "Brown", "White"];
 
 const topppingList = [
-  'None',
-  'StripesWhite',
-  'StripesBrown',
-  'StripesRainbow',
-  'StripesPretzelDAO',
-  'SprinklesWhite',
-  'SprinklesBrown',
-  'SprinklesRainbow',
-  'SprinklesPretzelDAO',
-  'DotsWhite',
-  'DotsBrown',
-  'DotsRainbow',
-  'DotsPretzelDAO',
-]
+  "None",
+  "StripesWhite",
+  "StripesBrown",
+  "StripesRainbow",
+  "StripesPretzelDAO",
+  "SprinklesWhite",
+  "SprinklesBrown",
+  "SprinklesRainbow",
+  "SprinklesPretzelDAO",
+  "DotsWhite",
+  "DotsBrown",
+  "DotsRainbow",
+  "DotsPretzelDAO",
+];
 
 function camelToUnderscore(key) {
-  var result = key.replace(/([A-Z])/g, ' $1')
-  return result.split(' ').join('_').toLowerCase()
+  var result = key.replace(/([A-Z])/g, " $1");
+  return result.split(" ").join("_").toLowerCase();
 }
 
-console.log(camelToUnderscore('ItemName'))
-
 function buildImages(bg, half, salt, coating, topping) {
-  let ret = []
+  let ret = [];
   ret.push({
     input: `./sugar-pretzels/${bg}.png`,
     top: 0,
     left: 0,
-  })
+  });
   //quick-fix to make it possible without parsing arguments
   ret.push({
     input: `./sugar-pretzels/classic.png`,
     top: 0,
     left: 0,
-  })
+  });
 
-  const prefix = half ? 'half' : 'full'
+  const prefix = half ? "half" : "full";
   if (coating > 0) {
     ret.push({
       input: `./sugar-pretzels/${
@@ -45,7 +43,7 @@ function buildImages(bg, half, salt, coating, topping) {
       }.png`,
       top: 0,
       left: 0,
-    })
+    });
   }
   if (topping > 0) {
     ret.push({
@@ -54,21 +52,21 @@ function buildImages(bg, half, salt, coating, topping) {
       }.png`,
       top: 0,
       left: 0,
-    })
+    });
   }
   if (salt) {
     ret.push({
       input: `./sugar-pretzels/salt.png`,
       top: 0,
       left: 0,
-    })
+    });
   }
-  console.log('making pretzel:', ret)
-  return ret
+  console.log("making pretzel:", ret);
+  return ret;
 }
 
 module.exports = {
   buildImages,
   coatingList,
   topppingList,
-}
+};
